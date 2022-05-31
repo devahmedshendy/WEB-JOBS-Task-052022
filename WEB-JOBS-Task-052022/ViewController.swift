9360f57bd2186e15e3dc49899737b2fc82721f69
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2022-05-31 16:15:55 +0200

    Initial Commit

---
+++ WEB-JOBS-Task-052022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WEB-JOBS-Task-052022
+//
+//  Created by Ahmed Shendy on 31/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
